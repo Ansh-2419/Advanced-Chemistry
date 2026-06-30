21eb1bcb78054bdef0b936d7caaa454dc2441de8
Ansh-2419 <archanapadh@gmail.com>
2026-06-30 18:12:08 +0530

    Last Sync: 2026-06-30 18:12 (Mobile)

--- BP/scripts/config/recipes/added/insert_infuser.js
+++ BP/scripts/config/recipes/added/insert_infuser.js
@@ -8,12 +8,16 @@ world.afterEvents.worldLoad.subscribe(() => {
             output: "utilitycraft:duranium_ingot",
             required: 4
         },
-        "minecraft:water_bucket|minecraft:brown_mushroom": {
+        "minecraft:brown_mushroom|minecraft:water_bucket": {
             output: "utilitycraft:water_bucket_custom",
             required: 4
         },
-        "minecraft:water_bucket|minecraft:red_mushroom": {
+        "minecraft:red_mushroom|minecraft:water_bucket": {
             output: "utilitycraft:water_bucket_custom",
+            required: 4
+        },
+        "utilitycraft:lithium_dust|utilitycraft:steel_ingot": {
+            output: "utilitycraft:electrum_ingot",
             required: 4
         }
     };
